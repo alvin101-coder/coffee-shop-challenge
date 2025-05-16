8f42268069bce71f9675783d98fa4b17d265320e
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-05-16 21:25:22 +0300

    feat: add order relationship methods to Customer and Coffee

--- coffee.py
+++ coffee.py
@@ -3,8 +3,15 @@ class Coffee:
         if isinstance(name, str) and len(name) >= 3:
             self._name = name
         else:
-            raise ValueError("Name must be a sting with at least 3 characters")
+            raise ValueError("Name must be a string with at least 3 characters.")
+        self._orders = []
 
     @property
     def name(self):
-        return self._name        
+        return self._name
+
+    def orders(self):
+        return self._orders.copy()
+
+    def customers(self):
+        return list({order.customer for order in self._orders})
--- customer.py
+++ customer.py
@@ -1,15 +1,23 @@
-class customer:
+from order import Order  # Prevent circular import in actual use
+
+class Customer:
     def __init__(self, name):
         self.name = name
+        self._orders = []
 
     @property
-    def name (self):
+    def name(self):
         return self._name
 
     @name.setter
     def name(self, value):
         if isinstance(value, str) and 1 <= len(value) <= 15:
             self._name = value
-
         else:
-            raise ValueError ("Name must be a string between 1 and 15 characters.")       
+            raise ValueError("Name must be a string between 1 and 15 characters.")
+
+    def orders(self):
+        return self._orders.copy()
+
+    def coffees(self):
+        return list({order.coffee for order in self._orders})
